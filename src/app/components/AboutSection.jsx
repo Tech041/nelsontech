ed08877e44abfd854a506329761a65d36a3ee361
nelson <chinwubanelson@gmail.com>
2025-03-11 18:31:37 +0100

    updated

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -55,20 +55,22 @@ const TAB_DATA = [
         </h1>
         <h2 className=""> Role: React Developer </h2>
         <p className="">
-          Develop scalable, responsive and dynamic web applications using React,
-          Next.js, JavaScript, TypeScript and Tailwind CSS. Use Redux and
-          Context API for global state management. • Integrate REST APIs and
-          other third-party services.
+          Developed scalable, responsive, and dynamic web applications which
+          include e-commerce, learning management system, SaaS UI, loan apps
+          etc. Utilizing React, Next.js, JavaScript, TypeScript, and Tailwind
+          CSS. Utilized Redux and Context API for efficient global state
+          management. • Consumed REST APIs and third-party services to enhance
+          application functionality and performance.
         </p>
         <hr className="my-10" />
         <h1 className="text-purple-500 text-2xl ">Upwork [2020 to current]</h1>
         <h2 className=""> Role: Front End Developer</h2>
         <p className="">
-          Developed and maintained various modules of complex web applications,
-          employing React.js/Next.js and TypeScript and consuming .Net, Django,
-          Spring Boot and REST APIs. • Integrated third-party APIs and services
-          to extend application functionalities, improving the overall user
-          experience.
+          Designed, developed, and maintained key modules of complex web
+          applications like e-commerce, hospital admin dashboard etc. Using
+          React.js/Next.js and TypeScript, while integrating .NET, Django,
+          Spring Boot, and REST APIs. Implemented third-party APIs and services
+          to enhance functionality and optimize the user experience.
         </p>
         <hr className="my-10" />
         <h1 className="text-purple-500 text-2xl ">Fiverr [2019 to 2020]</h1>
@@ -76,19 +78,21 @@ const TAB_DATA = [
         <p className="">
           Developed and maintained various modules of complex web applications,
           employing React.js/Next.js and TypeScript and consuming .Net, Django,
-          Spring Boot and REST APIs. • Integrated third-party APIs and services
-          to extend application functionalities, improving the overall user
+          Spring Boot and REST APIs. Integrated third-party APIs and services to
+          extend application functionalities, improving the overall user
           experience.
         </p>
         <hr className="my-10" />
         <h1 className="text-purple-500 text-2xl ">Megabon [2019]</h1>
         <h2 className=""> Role: Intern Developer</h2>
         <p className="">
-          Developed and maintained various modules of a complex web application,
-          employing React.js/Next.js and TypeScript for the frontend and
-          consumed .Net, Django, Spring Boot and REST APIs , Integrated
-          third-party APIs and services to extend application functionalities,
-          improving the overall user experience.
+          Worked closely with UX/UI designers to transform design mockups into
+          pixel-perfect, responsive user interfaces which include gym app,
+          e-commerce and LMS applications. Using React, Next.js,
+          TypeScript/JavaScript, Tailwind CSS, Redux, and other front-end
+          technologies. • Implemented server-side rendering (SSR) and static
+          site generation (SSG) with Next.js to improve application performance
+          and SEO. Integrated and consumed REST APIs to enhance functionality.
         </p>
       </div>
     ),
