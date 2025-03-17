5bb6790679e79b335e135c49c6c3c02bd51a8770
nelson <chinwubanelson@gmail.com>
2025-03-17 11:42:39 +0100

    updated favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,20 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Neltech | Portfolio',
-  description: 'My Portfolio website',
-}
+  title: "Neltech | Portfolio",
+  description: "My Portfolio website",
+  icons: {
+    icon: "/images/hero.jpg",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
