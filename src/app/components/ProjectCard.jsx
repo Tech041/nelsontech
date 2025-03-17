6987982cd2fa06c2bc59330f5a27a198f42cae62
nelson <chinwubanelson@gmail.com>
2025-03-17 11:11:49 +0100

    updated

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -8,25 +8,25 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, stack }) => {
       <div
         className="h-52 md:h-72 rounded-t-xl relative group"
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      >
-        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
+      ></div>
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
+        <div className="flex items-center justify-between">
+          <h5 className="text-xl font-semibold mb-2">{title}</h5>
           <Link
             href={gitUrl}
             target="_blank"
-            className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            className="text-green-600 font-bold"
           >
-            <span className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-green-500  font-semibold flex justify-center items-center ">
-              View
-            </span>
+            Live View
           </Link>
         </div>
-      </div>
-      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
-        <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
-        <p className="text-orange-700 flex  flex-wrap gap-1">
-          {stack.map((item,index)=>(
-            <span key={index} className="px-2 py-1 rounded-md">{item}</span>
+        <p className=" pt-1 ">Technologies used are::</p>
+        <p className="text-orange-700 flex  flex-wrap ">
+          {stack.map((item, index) => (
+            <span key={index} className="py-1 pr-3 rounded-md">
+              {item}
+            </span>
           ))}
         </p>
       </div>
