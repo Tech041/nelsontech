8d8f6800768bcfaa1faa3beb37c877d1927b660f
nelson <chinwubanelson@gmail.com>
2025-04-18 19:08:04 +0100

    updated profile pic

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -49,9 +49,9 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero.jpg"
+              src="/images/my_pic.jpg"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[250px] h-[250px] rounded-full"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-[250px] h-[250px] rounded-full object-cover"
               width={250}
               height={250}
             />
